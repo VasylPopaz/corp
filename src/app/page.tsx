085c680fe56e04408a1aa999966f15e5e57f84f8
VasylPopaz <pafos1997@gmail.com>
2024-08-30 15:23:53 +0200

    add bg images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
+import { Hero } from "@/components";
+
+import homeImg from "public/home.webp";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <Hero
+      imgData={homeImg}
+      imgAlt="car factory"
+      title="Professional Cloud Hosting"
+    />
+  );
 };
 export default Home;
